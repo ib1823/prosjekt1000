b732a4655f945286960a00c60a680c8107f6e03d
stvne1 <shifty.ankles10@gmail.com>
2025-04-24 12:50:22 +0200

    Endret

--- BliSikker/Fag/Emner/Fag/Fag.js
+++
@@ -1,9 +0,0 @@
- // Dark mode toggle
- document.addEventListener('DOMContentLoaded', function() {
-    const modeToggle = document.getElementById('mode-toggle');
-    
-    modeToggle.addEventListener('change', function() {
-        document.body.classList.toggle('dark-mode');
-    });
-});
-
